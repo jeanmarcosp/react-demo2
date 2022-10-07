75af4d0d6bb533ea51948648be221f5aef49580c
Jeanmarcos Perez <90585350+jeanmarcosp@users.noreply.github.com>
2022-10-06 20:16:18 -0400

    edited app in css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Jeanmarcos";
+  let varname = null;
+
+  if (name == "Jeanmarcos") {
+    varname = (<p> this is jeanmarcos </p>);
+  }
+
+  var namecheck = (name == "Jeanmarcos") ? (<p>hi my name is {name}</p>) : (<p>my  name is not {name}</p>);
+
+  const buttonclick = () => {
+    console.log("I clicked the button");
+  }
+
+  const onChangeFunction = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p>hi my name is {name}</p>
+
+        {varname}
+
+        {namecheck}
+
+        <button onClick={buttonclick}>I am a button</button>
+
+        <input type="text" onChange={onChangeFunction}/>
+
       </header>
     </div>
   );
